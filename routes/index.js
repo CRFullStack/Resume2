aca3df39fe89d19bbf6e410fcb5b7110c7d8b2bc
CRFullStack <hobdy.chris02@gmail.com>
2018-11-01 00:10:29 -0400

    finish experience section, repostitioned navbar items

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    navBarItems: ['About', 'Experience', 'Education', 'Skills', 'Interests', 'Awards' ],
-    about: {
+    // add nav bar items to the navBarItems arr
+    navBarItems: ['About', 'Experience', 'Education', 'Awards', 'Interests', 'Skills' ],
+    about: { // the about section 
       firstName: 'Christopher',
       lastName: 'Hobdy',
       stAddress: '4140 Windsor Castle Way',
@@ -17,16 +18,16 @@ router.get('/', function(req, res, next) {
       summary: 'Chris I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.',
       socialMedia:[
           {
-          icon: 'fa-linkedin-in',
+          icon: 'fa-linkedin-in', //linkedin icon
           url: '#'
         },
         {
-          icon: 'fa-github',
+          icon: 'fa-github', //github icon
           url: 'https://github.com/CRFullStack'
         }
       ]
     },
-    education: [
+    education: [ //the education section. Add as many dictionaries as needed to the education obj
       {
         schoolName: 'Huntingdon College',
         degree: 'Bachelor',
@@ -41,9 +42,29 @@ router.get('/', function(req, res, next) {
         gpa: '3.0',
         datesEnrolled: 'May 10th - June 22nd'
       }
+    ],
+    experience:[ // the experience section. Add as many dictionaries as needed to the experience obj
+      {
+        title: 'web designer',
+        company: 'cfa',
+        summary: 'Im the best',
+        date: '01/2009 - current'
+      },
+      {
+        title: 'web designer2',
+        company: 'cfa',
+        summary: 'Im the best2',
+        date: '01/2009 - current'
+      },
+      {
+        title: 'web designer3',
+        company: 'cfa',
+        summary: 'Im the best3',
+        date: '01/2009 - current'
+      }
     ]
     
-  });
-});
+  }); //end render
+}); //end get
 
 module.exports = router;
